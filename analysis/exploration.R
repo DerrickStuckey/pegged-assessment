3344844fcaf6ed66679e565bee1d019711c2b329
DerrickStuckey <derrickstuckey@gmail.com>
2016-07-16 10:10:14 -0400

    checkpoint commit

---
+++ analysis/exploration.R
@@ -0,0 +1,13 @@
+## Derrick Stuckey
+## Pegged Software Assessment
+## 07/16/2016
+
+# load the data
+data_exercise_applicants <- read.csv("../data/data_exercise_applicants.csv")
+data_exercise_hires <- read.csv("../data/data_exercise_hires.csv")
+
+dim(data_exercise_applicants)
+dim(data_exercise_hires)
+
+View(data_exercise_applicants)
+View(data_exercise_hires)
